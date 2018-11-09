d4752dcef98cedb2ab3d5e5eb6c1fb63cf251c73
liangzhicheng <liangzhicheng@meizu.com>
2018-11-09 18:31:58 +0800

    增加路由入口

--- ai-cloud-gateway/src/main/java/com/ai/gategory/GategoryApplication.java
+++ ai-cloud-gateway/src/main/java/com/ai/gategory/GategoryApplication.java
@@ -1,4 +1,4 @@
-package com.ai.gateway;
+package com.ai.gategory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-public class GatewayApplication {
+public class GategoryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(GategoryApplication.class, args);
     }
 }
